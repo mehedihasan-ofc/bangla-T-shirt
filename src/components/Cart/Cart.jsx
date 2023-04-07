a73d52adbf9fba2b5b2e5122e2ba4c52513cdc09
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-04-07 12:02:33 +0600

    added conditional rendering and conditional css styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+
+    let message;
+    if(cart.length === 0) {
+        message = "Please add some products"
+    }
+    else {
+        message = <div>
+            <h3>Boroloxx</h3>
+            <p><small>Thanks for giving your money</small></p>
+        </div>
+    }
+
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'purple' : 'orange'}>Order Summary: {cart.length}</h2>
+            {/* CONDITIONAL CSS STYLE */}
+            <p className={`bold ${cart.length === 3 ? "teal" : "yellow"}`}>Something</p>
+
+            {/*==========> CONDITIONAL RENDERING <==============*/}
+            {/* {cart.length > 2 ? <span>Aro kino</span> : <span>Fokira</span>} */}
+            {message}
+            {cart.length === 2 && <p>Double Bonanza!!!</p>}
+            {cart.length === 3 || <h3>Tinta to hoilo na</h3>}
 
             {
                 cart.map(tshirt => <p
@@ -17,3 +38,17 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
 };
 
 export default Cart;
+
+/**
+ * Conditional Rendering
+ * 01. use if else to set a variable that will contain an element, components
+ * 02. ternary operator: condition ? 'for true' : 'false'
+ * 03. Logical &&: (if the condition is true then the next thing will be displayed)
+ * 04. Logical || : (if the condition is false then the next thing will be displayed)
+*/
+
+/**
+ * Conditional CSS Style
+ * 01. use ternary
+ * 02. ternary insie template string
+*/
---
+++ src/components/Grandapa/Grandapa.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grandapa = () => {
+    return (
+        <div>
+            <h2>Grandapa</h2>
+        </div>
+    );
+};
+
+export default Grandapa;
